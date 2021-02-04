2d6da3eb35622473a28da1a0b97ff1fc88945388
Clarissa Elbo <celbo@ucsd.edu>
2021-02-04 11:03:18 -0800

    added Javascript and CSS

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,20 @@ var data = require('../data.json');
 
 exports.view = function(request, response){
 	console.log(data);
-	response.render('index');
+	response.render('index', data);
 };
+
+function initializePage() {
+    $('#testjs').click(function(e) {
+            $('.jumbotron h1').text("Javascript is connected");
+            });
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+  }
